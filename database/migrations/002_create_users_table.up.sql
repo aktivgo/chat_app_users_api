0a4b44a747e83d106589c890965fa5b9b285e0a2
aktivgo <AktivGo01062001@gmail.com>
2021-10-21 12:44:52 +0300

    Добавил аватар и изменил проверку данных

--- database/migrations/002_create_users_table.up.sql
+++ database/migrations/002_create_users_table.up.sql
@@ -1,10 +1,10 @@
 create table if not exists users
 (
-    id       int(11)      not null primary key auto_increment,
-    login    varchar(255) not null ,
-    password varchar(255) not null,
-    fullName varchar(255) null,
-    email varchar(255) null,
-    confirmed boolean null default false,
-    avatar varchar(255) null
+    id        int(11)      not null primary key auto_increment,
+    login     varchar(255) not null,
+    password  varchar(255) not null,
+    fullName  varchar(255) null,
+    email     varchar(255) null,
+    confirmed boolean      null default false,
+    avatar    varchar(255) null
 );
--- includes/signup.php
+++ includes/signup.php
@@ -9,6 +9,7 @@ use aktivgo\chat\database\Database;
 $fullName = $_POST['fullName'];
 $login = $_POST['login'];
 $email = $_POST['email'];
+$avatar = null;
 $password = $_POST['password'];
 $passwordConfirm = $_POST['passwordConfirm'];
 
@@ -54,12 +55,21 @@ if ($password != $passwordConfirm) {
     die();
 }
 
+if (isset($_FILES['avatar'])) {
+    $avatar = 'uploads/' . time() . $_FILES['avatar']['name'];
+    if (!move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $avatar)) {
+        HttpResponse::toSendResponse(['Ошибка при загрузке изображения'], 400);
+        die();
+    }
+}
+
 $password = md5($password);
 
 UserController::addUser($db, [
     'fullName' => $fullName,
     'login' => $login,
     'email' => $email,
+    'avatar' => $avatar,
     'password' => $password,
 ]);
 
