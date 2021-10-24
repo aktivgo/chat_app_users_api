db3bac9e688b8479b9996b02f0f52617dc2c2340
aktivgo <AktivGo01062001@gmail.com>
2021-10-24 18:45:43 +0300

    Добавил автоикремент

--- database/migrations/003_create_messages_table.up.sql
+++ database/migrations/003_create_messages_table.up.sql
@@ -1,6 +1,6 @@
 create table if not exists messages
 (
-    id       int(11)       not null primary key,
-    userName varchar(255)  not null,
+    id       int(11)       not null primary key auto_increment,
+    userName varchar(255)  null,
     message  varchar(1024) null
 );
