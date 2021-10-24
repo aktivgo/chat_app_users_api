5ccd7c5b98c2f86607a7f46961ccd4f7cb94781b
aktivgo <AktivGo01062001@gmail.com>
2021-10-24 18:16:43 +0300

    Убрал поле time

--- database/migrations/003_create_messages_table.up.sql
+++ database/migrations/003_create_messages_table.up.sql
@@ -2,6 +2,5 @@ create table if not exists messages
 (
     id       int(11)       not null primary key,
     userName varchar(255)  not null,
-    message  varchar(1024) null,
-    time     time          null
+    message  varchar(1024) null
 );
